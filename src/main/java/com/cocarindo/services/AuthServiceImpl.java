baf32d3ab40166cd402c11ceb692988a88b7156c
Luis <luish.vargas09@gmail.com>
2024-06-10 00:01:55 -0600

    Create an Admin account, password enconde

--- src/main/java/com/cocarindo/services/AuthServiceImpl.java
+++ src/main/java/com/cocarindo/services/AuthServiceImpl.java
@@ -3,7 +3,9 @@ package com.cocarindo.services;
 import com.cocarindo.entities.User;
 import com.cocarindo.enums.UserRole;
 import com.cocarindo.repositories.UserRepository;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -14,7 +16,19 @@ public class AuthServiceImpl {
 
     private final UserRepository userRepository;
 
+    @PostConstruct
     private void createAnAdminAccount() {
         Optional<User> optionalUser = userRepository.findByUserRole(UserRole.ADMIN);
+
+        if (optionalUser.isEmpty()) {
+            User newAdmin = new User();
+            newAdmin.setName("Admin");
+            newAdmin.setEmail("admin@test.com");
+            newAdmin.setPassword(new BCryptPasswordEncoder().encode("admin"));
+            newAdmin.setUserRole(UserRole.ADMIN);
+            userRepository.save(newAdmin);
+        } else {
+            System.out.println("Admin account already exists!");
+        }
     }
 }
